74eeeb33dd5606c795246c7dcfccd78ce01c7c39
jimouz <jimouzounis@gmail.com>
2026-01-27 13:30:41 +0200

    UI style update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,32 @@
 import ToolCardList from "../components/ToolCardList.jsx";
 import HomeHero from "../components/HomeHero";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 export default function Home() {
     return (
-        <Container maxWidth="md" sx={{ my: 4, minHeight: "78vh" }}>
+        <Container 
+            maxWidth="md"
+            sx={(theme) => ({
+                my: 4,
+                minHeight: "78vh",
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+            })}
+        >
             <HomeHero />
+
+            <Typography
+                variant="h5"
+                sx={{
+                    my: 2,
+                    letterSpacing: "1px",
+                    opacity: 0.8,
+                    textAlign: "center",
+                }}
+            >
+                Tools
+            </Typography>
             <ToolCardList />
         </Container>
     );
